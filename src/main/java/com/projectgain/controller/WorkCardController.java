4578a506af70f001e07953a66cd17d82fa200b70
BadalSabin <55592534+BadalSabin@users.noreply.github.com>
2023-03-11 21:29:59 +0545

    Implemented workout timer adder card

---
+++ src/main/java/com/projectgain/controller/WorkCardController.java
@@ -0,0 +1,32 @@
+package com.projectgain.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+public class WorkCardController {
+
+    @FXML
+    private TextField cardTitleTextField;
+
+    @FXML
+    private ToggleButton cardSetToggleButton;
+
+    @FXML
+    private ToggleButton cardTimeToggleButton;
+
+    @FXML
+    private Button cardDeleteButton;
+
+    @FXML
+    private Button cardCopyButton;
+
+    @FXML
+    private Button cardSaveButton;
+
+    @FXML
+    private Label cardBGColorSelectLabel;
+
+    @FXML
+    private ColorPicker cardBGColorSelectColorPicker;
+
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.projectgain.projectgain {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.projectgain.projectgain to javafx.fxml;
+    exports com.projectgain.projectgain;
+}
