f470c5ca9562b3f712539bb6e555abf5eddb1837
BadalSabin <55592534+BadalSabin@users.noreply.github.com>
2023-03-27 21:30:23 +0545

    Fixed unscrollable bug on adding new work group and made UI more discernible

--- src/main/java/com/projectgain/controller/WorkRoutineController.java
+++ src/main/java/com/projectgain/controller/WorkRoutineController.java
@@ -3,7 +3,7 @@ package com.projectgain.controller;
 import com.projectgain.FxmlViewLoader;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 
@@ -12,11 +12,17 @@ public class WorkRoutineController {
     private VBox workGroupDisplayVBox;
 
     @FXML
+    private ScrollPane workGroupDisplayScrollPane;
+
+    @FXML
     private Button addNewWorkGroupButton;
 
     @FXML
     protected void onAddNewWorkGroupButtonClicked(){
         Pane workCardGroup = FxmlViewLoader.getPage("WorkCardGroup");
         workGroupDisplayVBox.getChildren().add(workCardGroup);
+        workGroupDisplayVBox.heightProperty().addListener(
+                observable -> workGroupDisplayScrollPane.setVvalue(1D)
+        );
     }
 }
