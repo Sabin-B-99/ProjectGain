0c1864833d06aa210c29c6ec45bed4f55ac202f5
BadalSabin <55592534+BadalSabin@users.noreply.github.com>
2023-04-02 17:54:02 +0545

    Implemented view toggle based on type of workout (rep/timed) and added custom TimeSpinner

--- src/main/java/com/projectgain/controllers/WorkCardController.java
+++ src/main/java/com/projectgain/controllers/WorkCardController.java
@@ -1,14 +1,21 @@
 package com.projectgain.controllers;
 
+import com.projectgain.customcontrol.TimeSpinner;
 import com.projectgain.manager.WorkRoutineManager;
+import com.projectgain.models.WorkCard;
 import com.projectgain.views.ViewFactory;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
 import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 
 import java.net.URL;
+import java.time.LocalTime;
 import java.util.ResourceBundle;
 
 /**
@@ -18,6 +25,7 @@ import java.util.ResourceBundle;
 public class WorkCardController extends BaseController implements Initializable {
 
     private WorkRoutineManager manager;
+    private WorkCard cardModel;
     @FXML
     private AnchorPane workCardRootAnchorPane;
 
@@ -25,10 +33,15 @@ public class WorkCardController extends BaseController implements Initializable 
     private TextField cardTitleTextField;
 
     @FXML
-    private ToggleButton cardSetToggleButton;
+    private ToggleButton cardRepToggleButton;
 
+    private Spinner<Integer> repSpinner;
     @FXML
     private ToggleButton cardTimeToggleButton;
+    private TimeSpinner timeSpinner;
+
+    @FXML
+    private HBox timeRepControlHBox;
 
     @FXML
     private Button cardDeleteButton;
@@ -47,16 +60,18 @@ public class WorkCardController extends BaseController implements Initializable 
         super(fxmlViewName, viewFactory);
     }
 
-    public WorkCardController(String fxmlViewName, ViewFactory viewFactory, WorkRoutineManager manager) {
+    public WorkCardController(String fxmlViewName, ViewFactory viewFactory, WorkRoutineManager manager, WorkCard cardModel) {
         super(fxmlViewName, viewFactory);
         this.manager = manager;
+        this.cardModel = cardModel;
+        this.timeSpinner = new TimeSpinner();
+        this.repSpinner = new Spinner<>();
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        Color cardColor = manager.generateWorkCardColor();
-        cardBGColorSelectColorPicker.setValue(cardColor);
-        workCardRootAnchorPane.setStyle("-fx-background-color: " + viewFactory.getColorHex(cardColor));
+        performCardInitializationTasks();
+        addWorkCardModelListeners();
     }
     @FXML
     protected void onCardDeleteButtonPressed(){
@@ -72,5 +87,57 @@ public class WorkCardController extends BaseController implements Initializable 
     @FXML
     protected void onCardCopyButtonClicked(){
         manager.copyWorkCard(workCardRootAnchorPane);
+        //debug code //TODO: Delete these codes later
+        String color = viewFactory.getColorHex(manager.generateWorkCardColor());
+        cardModel.setTitle(color);
+    }
+
+    private void addWorkCardModelListeners(){
+        cardModel.titleProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observableValue, String s, String t1) {
+                cardTitleTextField.setText(cardModel.getTitle());
+                System.out.println("loop: model");
+            }
+        });
+
+        cardModel.colorHexCodeProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observableValue, String s, String t1) {
+                cardBGColorSelectColorPicker.setValue(Color.web(cardModel.getColorHexCode()));
+                workCardRootAnchorPane.setStyle("-fx-background-color: " + cardModel.getColorHexCode());
+            }
+        });
+    }
+
+    private void performCardInitializationTasks(){
+        Color cardColor = manager.generateWorkCardColor();
+        cardBGColorSelectColorPicker.setValue(cardColor);
+        workCardRootAnchorPane.setStyle("-fx-background-color: " + viewFactory.getColorHex(cardColor));
+
+        SpinnerValueFactory<Integer> repSpinnerValueFactory = new SpinnerValueFactory.IntegerSpinnerValueFactory(1, 10);
+        repSpinnerValueFactory.setValue(1);
+        repSpinner.setValueFactory(repSpinnerValueFactory);
+
+        cardTimeToggleButton.setSelected(true);
+        timeRepControlHBox.getChildren().add(timeSpinner);
+
+        cardRepToggleButton.selectedProperty().addListener(new ChangeListener<Boolean>() {
+            @Override
+            public void changed(ObservableValue<? extends Boolean> observableValue, Boolean oldVal, Boolean newVal) {
+                repSpinner.getValueFactory().setValue(1);
+                if(newVal){
+                    timeSpinner.getValueFactory().setValue(LocalTime.of(0,0,3));
+                    timeRepControlHBox.getChildren().clear();
+                    timeRepControlHBox.getChildren().add(repSpinner);
+                    timeRepControlHBox.setSpacing(3);
+                    timeRepControlHBox.getChildren().add(timeSpinner);
+                }else {
+                    timeSpinner.getValueFactory().setValue(LocalTime.of(0,0,0));
+                    timeRepControlHBox.getChildren().clear();
+                    timeRepControlHBox.getChildren().add(timeSpinner);
+                }
+            }
+        });
     }
 }
