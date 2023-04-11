2365af92e953cac60cc93be8a4767bb5caa333cd
BadalSabin <55592534+BadalSabin@users.noreply.github.com>
2023-04-11 22:12:34 +0545

    Added methods to initialize, save and manipulate different model types

--- src/main/java/com/projectgain/manager/WorkRoutineManager.java
+++ src/main/java/com/projectgain/manager/WorkRoutineManager.java
@@ -1,11 +1,16 @@
 package com.projectgain.manager;
 
+import com.projectgain.models.WorkCard;
+import com.projectgain.models.WorkGroup;
+import com.projectgain.models.WorkRoutine;
+import com.projectgain.models.WorkType;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.collections.ObservableMap;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 
+import java.util.List;
 import java.util.Random;
 
 public class WorkRoutineManager {
@@ -15,11 +20,12 @@ public class WorkRoutineManager {
     private static final Color[] WORK_CARD_INITIAL_AVAILABLE_COLORS = {
             Color.RED, Color.YELLOW, Color.BLUE, Color.GREEN
     };
-
     private final ObservableList<Pane> landingWindowActivePanes;
     private final ObservableList<Pane> workGroupsPaneList;
     private final ObservableMap<Integer, ObservableList<Pane>> workCardsList;
+    private WorkRoutine workRoutineModel;
 
+    private int indexOfLastWorkGroupOnWhichAddBtnPressed;
     private int totalWorkCardGroup;
 
     public WorkRoutineManager() {
@@ -49,6 +55,7 @@ public class WorkRoutineManager {
         int indexOfPaneBeingRemoved = getWorkGroupIndex(workCardGroupRootAnchorPane);
         this.workGroupsPaneList.remove(workCardGroupRootAnchorPane);
         this.getWorkCardsList().remove(indexOfPaneBeingRemoved);
+        this.workRoutineModel.getWorkGroupList().remove(indexOfPaneBeingRemoved);
         setTotalWorkCardGroup(getTotalWorkCardGroup() - 1);
     }
 
@@ -71,7 +78,7 @@ public class WorkRoutineManager {
         }
     }
 
-    private ObservableList<Pane> findWorkCardGroup(Pane workCardPane){
+    public ObservableList<Pane> findWorkCardGroup(Pane workCardPane){
         for (Integer key: workCardsList.keySet()) {
             if(workCardsList.get(key).contains(workCardPane)){
                 return workCardsList.get(key);
@@ -79,6 +86,16 @@ public class WorkRoutineManager {
         }
         return null;
     }
+
+    public int findWorkCardParentWorkGroupIndex(Pane workCardPane){
+        for (Integer key: workCardsList.keySet()) {
+            if(workCardsList.get(key).contains(workCardPane)){
+                return key;
+            }
+        }
+        return -1;
+    }
+
 
     public void copyWorkCard(Pane workCardPane) {
         System.out.println("TODO: work will be done later. Need to code own logic to copy node");
@@ -100,5 +117,51 @@ public class WorkRoutineManager {
         this.workCardsList.clear();
         this.workGroupsPaneList.clear();
         this.landingWindowActivePanes.remove(workRoutineFormRootPane);
+    }
+    public void setWorkRoutineModel(WorkRoutine workRoutineModel) {
+        this.workRoutineModel = workRoutineModel;
+    }
+
+    public WorkRoutine getWorkRoutineModel() {
+        return workRoutineModel;
+    }
+
+    public int getIndexOfLastWorkGroupOnWhichAddBtnPressed() {
+        return indexOfLastWorkGroupOnWhichAddBtnPressed;
+    }
+
+    public void setIndexOfLastWorkGroupOnWhichAddBtnPressed(int indexOfLastWorkGroupOnWhichAddBtnPressed) {
+        this.indexOfLastWorkGroupOnWhichAddBtnPressed = indexOfLastWorkGroupOnWhichAddBtnPressed;
+    }
+
+    public List<WorkGroup> getWorkRoutineWorkGroups(){
+        return this.workRoutineModel.getWorkGroupList();
+    }
+
+    public List<WorkCard> getWorkCardsOfWorkGroupAtIndex(int workGroupIndex){
+        return this.workRoutineModel.getWorkGroupList().get(workGroupIndex).getWorkCardList();
+    }
+
+    //TODO: Add database support and remove debug codes
+    public void performRoutineSaveOperations() {
+        System.out.println("Routine title: " + workRoutineModel.getTitle());
+        List<WorkGroup> workGroups = workRoutineModel.getWorkGroupList();
+        List<WorkCard> workCards = null;
+
+        for (WorkGroup wg: workGroups) {
+            workCards = wg.getWorkCardList();
+            System.out.println("************** Work Group Sets *************: "+ wg.getSets());
+            for (WorkCard wc: workCards) {
+                System.out.println("Card Title: " + wc.getTitle());
+                System.out.println("Card Color: " + wc.getColorHexCode());
+                if(wc.getWorkType() == WorkType.TIMED){
+                    System.out.println("Card Time: " + wc.getTime());
+                }else{
+                    System.out.println("Card Reps: " + wc.getReps());
+                    System.out.println("Card TIme for each reps: " + wc.getTime());
+                }
+            }
+        }
+
     }
 }
